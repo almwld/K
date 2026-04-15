0712575da7f34bfbf646cf0a86131620c4223d1f
almwld <your-email@example.com>
2026-04-15 10:28:41 +0300

    🔧 تصحيح URL Supabase إلى القيمة الصحيحة ziqpohdxtemsmunnhlkm

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
   
   // تهيئة Supabase
   await Supabase.initialize(
-    url: 'https://ziqpohdxtermunnhlkm.supabase.co',
+    url: 'https://ziqpohdxtemsmunnhlkm.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppcXBvaGR4dGVtc211bm5obGttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3ODQzNDcsImV4cCI6MjA4NzM2MDM0N30.ABAg5YZSrrAtBTWATJ3eRTmo4BuZVyVlrMV1HZjRWs0',
   );
   
