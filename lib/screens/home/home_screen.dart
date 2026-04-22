75915b9f46fe1ecfec175c5659488b217585d5d8
almwld <your-email@example.com>
2026-04-22 20:05:43 +0300

    ✅ إصلاح SVG: إضافة imports وتحديث الأيقونات في جميع الشاشات

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
--- lib/screens/offers_screen.dart
+++ lib/screens/offers_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 
